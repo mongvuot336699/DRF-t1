a227e40c44fc5e9d84fc36a420723525234a3c21
hdtrung1 <hdtrung1@cmcglobal.vn>
2023-05-30 14:48:07 +0700

    done apis

--- apis/tests.py
+++ apis/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from books.models import Book
+
+
+class APITests(APITestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title = 'Django for APIs',
+            subtitle = 'Build web API with Python and Django',
+            author = 'William Vincent',
+            isbn = '1231239982',
+        )
+
+    def test_api_listview(self):
+        response = self.client.get(reverse('book_list'))
+        print(response)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertContains(response, self.book)
